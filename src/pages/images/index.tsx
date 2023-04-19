724be4ce134323ebc40146b208f1519953e9c6ca
newalvaro9 <alvaropobladoresteban9@gmail.com>
2023-04-19 21:05:10 +0200

    testing

--- src/pages/images/index.tsx
+++ src/pages/images/index.tsx
@@ -1,12 +1,31 @@
-export default function Index() {
-    return <></>
+import { useRouter } from "next/router";
+import connect from "../../../lib/database/database";
+import images from "../../../lib/database/models/images";
+import { GetServerSideProps } from "next";
+
+export default function Index({ newX }: { newX: Array<string> }) {
+
+    const router = useRouter();
+
+    return (
+        <>
+            {newX.map((x: string) => <h3 style={{ cursor: 'pointer' }} key={x} onClick={() => router.push(`/images/${x}`)}>{x}</h3>)}
+        </>
+    )
 }
 
-export const getServerSideProps = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+    await connect();
+    const queriedimages = await images.find();
+    const newX: Array<string> = [];
+
+    queriedimages.forEach(x => {
+        newX.push(x.link)
+    });
+
     return {
-        redirect: {
-            destination: '/',
-            permanent: false
+        props: {
+            newX
         }
     }
 }
