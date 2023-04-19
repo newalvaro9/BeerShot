fcaa498dce6486d20c85d4269d339299e188cf18
Álvaro <alvaropobladoresteban9@gmail.com>
2023-04-19 23:28:46 +0200

    name changed

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children, title, withoutContainer }: { children
     return (
         <>
             <Head>
-                <title>{title ? title : "Climbing Up"}</title>
+                <title>{title ? title : "BeerShoot"}</title>
                 {/* <meta name="description" content="" /> */}
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
