1c130689d44f1755c271e60ca7ba38395b72f47e
Álvaro <alvaropobladoresteban9@gmail.com>
2023-04-19 23:29:19 +0200

    no params redirect

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,35 +1,15 @@
 import { GetServerSideProps } from "next"
 import { getSession } from "next-auth/react"
-import connect from "../../../lib/database/database";
-import users from "../../../lib/database/models/users";
 
-export default function Profile({ userinfo }) {
-    return (
-
-    )
-}
+export default () => (<></>)
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
     const session = await getSession(context);
 
-    if (!session) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false,
-            },
-        };
-    }
-
-    await connect();
-
-    const user = await users.findOne({ email: session?.user?.email })
-    if(!user) 
-
     return {
-        props: {
-
+        redirect: {
+            destination: (session?.user as any)?.userid ? `/users/${(session?.user as any).userid}` : '/',
+            permanent: false,
         }
     }
 }
