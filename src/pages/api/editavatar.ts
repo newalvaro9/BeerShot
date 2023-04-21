32607753d2c943a06f1309b3aa2d6a1d7e24e086
Álvaro <alvaropobladoresteban9@gmail.com>
2023-04-21 15:15:40 +0200

    Edit avatar and biography

---
+++ src/pages/api/editavatar.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
+
+import connect from '../../../lib/database/database';
+import users from '../../../lib/database/models/users';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') return res.redirect('/404');
+
+    if (req.body) {
+        const session = await getServerSession(req, res, authOptions);
+        if (!session) return res.status(403).send("Unauthorized");
+
+        const { newAvatar } = req.body as {
+            newAvatar: string;
+        }
+
+        await connect();
+
+        try {
+            await users.findOneAndUpdate(
+                { userid: session.user.userid },
+                { avatar: newAvatar }
+            );
+        } catch (err) {
+            return res.status(500).send("Internal server error");
+        }
+
+        res.status(200).send("OK");
+    } else {
+        res.status(500).send("Internal server error");
+    }
+}
+
+export const config = {
+    api: {
+        responseLimit: '4mb',
+    },
+}
--- src/pages/users/[user].tsx
+++ src/pages/users/[user].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next"
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { getSession } from "next-auth/react"
 import Layout from "@/components/layout";
 
@@ -20,36 +21,199 @@ interface Props {
 
 export default function Profile({ userinfo, imageCollection, watchingMyProfile }: Props) {
 
+    const [showOverlayBio, setShowOverlayBio] = useState<boolean>(false)
+    const [showOverlayAvatar, setShowOverlayAvatar] = useState<boolean>(false)
+    const [newImage, setNewImage] = useState<string>("")
+
+    const [userAvatar, setUserAvatar] = useState<string>(userinfo.avatar || "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg")
+    const [userBio, setUserBio] = useState<string | null>(userinfo.biography || null)
+
+    const useRefNewBiography = useRef<HTMLTextAreaElement>(null)
+
+    const toggleOverlayVisibility = (overlay: string) => {
+        if (overlay === 'bio') {
+            setShowOverlayBio(prev => !prev)
+        }
+        else if (overlay === 'avatar') {
+            setShowOverlayAvatar(prev => !prev)
+        }
+    }
+
+    const closeOverlay = (overlay: string) => {
+        if (overlay === 'bio') {
+            setShowOverlayBio(false)
+        }
+        else if (overlay === 'avatar') {
+            setShowOverlayAvatar(false)
+        }
+    }
+
+    const handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files![0];
+
+        if (!["image/jpeg", "image/jpg", "image/png"].includes(file.type)) return alert("You must upload a supported file type.");
+
+        if (file) {
+            if (file.size > 2_000_000) return alert("Your image must not exceed 2mb size");
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+
+            reader.onload = () => {
+                setNewImage(reader.result as string);
+            };
+        }
+    };
+
+    const handleChangeBio = () => {
+        const data = {
+            newBio: useRefNewBiography!.current!.value
+        };
+
+        fetch('/api/editbio', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    setShowOverlayBio(false)
+                    setUserBio(useRefNewBiography!.current!.value)
+                } else {
+                    alert("There was an error setting your biography");
+                }
+            })
+            .catch(error => {
+                alert("There was an error setting your biography");
+            });
+    }
+
+    const handleChangeAvatar = () => {
+        const data = {
+            newAvatar: newImage
+        };
+
+        fetch('/api/editavatar', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(res => {
+                if (res.status === 200) {
+                    setShowOverlayAvatar(false)
+                    setUserAvatar(newImage)
+                } else {
+                    alert("There was an error sending your image");
+                }
+            })
+            .catch(error => {
+                alert("There was an error sending your image");
+            });
+    }
+
+    const dragElement = (elmnt: HTMLElement, elmnt2: HTMLElement) => {
+        if (!elmnt || !elmnt2) return
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+        elmnt2.onmousedown = dragMouseDown;
+
+        function dragMouseDown(e: MouseEvent) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e: MouseEvent) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+    if (typeof window !== 'undefined') {
+        const overlayBio = document.getElementById("overlay-bio");
+        const headerDragBio = document.getElementById("header-drag-bio");
+        const overlayAvatar = document.getElementById("overlay-avatar");
+        const headerDragAvatar = document.getElementById("header-drag-avatar");
+
+        if (overlayBio && headerDragBio && overlayAvatar && headerDragAvatar) {
+            dragElement(overlayBio, headerDragBio);
+            dragElement(overlayAvatar, headerDragAvatar);
+        }
+    }
+
     return (
         <Layout>
+
+            <div
+                style={{ display: showOverlayBio ? 'block' : 'none' }}
+                className={styles['overlay-bio']}
+                id="overlay-bio"
+            >
+                <a className={styles["close-overlay-btn"]} onClick={() => closeOverlay('bio')}>&times;</a>
+
+                <h4 id="header-drag-bio">Biography</h4>
+                <textarea
+                    placeholder="I am 23 years old..."
+                    defaultValue={userinfo.biography || undefined}
+                    name="bio"
+                    id="bio"
+                    maxLength={280}
+                    ref={useRefNewBiography}
+                />
+                <button type="button" className={buttons["button-2"]} style={{ width: '100%' }} onClick={handleChangeBio}>Change biography</button>
+            </div>
+
+            <div
+                style={{ display: showOverlayAvatar ? 'block' : 'none' }}
+                className={styles['overlay-avatar']}
+                id="overlay-avatar"
+            >
+                <a className={styles["close-overlay-btn"]} onClick={() => closeOverlay('avatar')}>&times;</a>
+
+                <h4 id="header-drag-avatar">Avatar</h4>
+                <input id="avatar" name="avatar" type="file" accept=".jpg, .jpeg, .png" onChange={handleFileSelect} />
+                <button type="button" className={buttons["button-2"]} style={{ width: '100%' }} onClick={handleChangeAvatar}>Change avatar</button>
+            </div>
+
+
             <div className={styles["div-user"]}>
-                {
-                    userinfo.avatar ? (
-                        <Image
-                            src={userinfo.avatar}
-                            className={styles['avatar-user']}
-                            alt={"Avatar"}
-                            width={150}
-                            height={150}
-                            draggable={false}
-                        />
-                    ) : (
-                        <Image
-                            src={"https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg"}
-                            className={styles['avatar-user']}
-                            alt={"Avatar"}
-                            width={150}
-                            height={150}
-                            draggable={false}
-                        />
-                    )
-                }
+
+                <Image
+                    src={userAvatar}
+                    className={styles['avatar-user']}
+                    alt={"Avatar"}
+                    width={150}
+                    height={150}
+                    draggable={false}
+                />
 
                 <div className={styles["username-bio"]}>
                     <h1 className={styles['username']}>{userinfo.username}</h1>
 
-                    {userinfo.biography ? (
-                        <p>{userinfo.biography}</p>
+                    {userBio ? (
+                        <p>{userBio}</p>
                     ) : (
                         watchingMyProfile ? (
                             <p>You do not have a biography.</p>
@@ -59,6 +223,13 @@ export default function Profile({ userinfo, imageCollection, watchingMyProfile }
                     )}
                 </div>
             </div>
+
+            {watchingMyProfile && (
+                <div className={styles["div-edits"]}>
+                    <button className={buttons["button-2"]} id="edit-avatar" onClick={() => toggleOverlayVisibility('avatar')}>Change avatar</button>
+                    <button className={buttons["button-2"]} id="edit-bio" onClick={() => toggleOverlayVisibility('bio')}>Change biography</button>
+                </div>
+            )}
 
             <hr className={styles["separator"]} style={{ marginTop: '5px', marginBottom: '7px' }} />
 
@@ -133,12 +304,18 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = await getSession(context);
     const watchingMyProfile = (session?.user as any)?.userid == context?.params?.user;
 
+    let imageBase64 = null;
+    if (user.avatar) {
+        const binaryData = user.avatar.buffer; // Get the buffer
+        imageBase64 = Buffer.from(binaryData, 'base64').toString('utf-8');
+    }
+
     return {
         props: {
             userinfo: {
                 email: user.email,
                 username: user.username,
-                avatar: user.avatar || null,
+                avatar: imageBase64,
                 biography: user.biography || null
             },
             imageCollection: imageCollection,
