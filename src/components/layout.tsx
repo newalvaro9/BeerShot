7581df3388d871aa91125dc631c383b9e30500d3
Álvaro <alvaropobladoresteban9@gmail.com>
2023-04-22 01:45:17 +0200

    description

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children, title }: { children: React.ReactNode,
         <>
             <Head>
                 <title>{title ? title : "BeerShoot"}</title>
-                {/* <meta name="description" content="" /> */}
+                <meta name="description" content="Upload your images and get a custom link to share it" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
             </Head>
